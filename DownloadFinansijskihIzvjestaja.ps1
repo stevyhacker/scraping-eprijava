39739b2dfcf2324f6fc2484a6749906df4aaf9a4
Stevan Bogosavljevic <stevyhacker@gmail.com>
2024-03-25 10:40:20 +0100

    Update companies list

--- DownloadFinansijskihIzvjestaja.ps1
+++ DownloadFinansijskihIzvjestaja.ps1
@@ -23,9 +23,18 @@ $companies.Add("02961717", "Media Monkeys")
 $companies.Add("03091627", "Codeus")
 $companies.Add("03084434", "Digital Control")
 $companies.Add("03165663", "Ridgemax")
+$companies.Add("03360962", "Infinum")
+$companies.Add("03191451", "Kodio")
+$companies.Add("03381447", "EPAM")
+$companies.Add("03413772", "First Line Software")
+$companies.Add("03374700", "Vega IT Omega")
+$companies.Add("03373398", "Quantox Technology")
+$companies.Add("03216446", "Ooblee")
+$companies.Add("03209296", "BIXBIT")
+$companies.Add("03367053", "GoldBear Technologies")
 
 # Formiranje CSV fajla za smjestanje rezultata
-Set-Content -Path ".\Results.csv" -Value '"Company","Year","TotalIncome","Profit","EmployeeCount","NetPayCosts","AveragePay"'
+Set-Content -Path "./Results.csv" -Value '"Company","Year","TotalIncome","Profit","EmployeeCount","NetPayCosts","AveragePay"'
 
 # Definisanje header-a zbog provizornog ID-a sesije
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
@@ -43,14 +52,14 @@ foreach ($company in $companies.GetEnumerator()) {
 	foreach ($taxpayer in $taxpayers) {
 		Write-Host "Pronadjen: $($taxpayer.PIB) - $($taxpayer.Naziv)"
 	}
-	
+
 	# Kreiranje pod-foldera za pravno lice
-	New-Item -ItemType Directory -Force -Path ".\$($company.Value)\" | Out-Null
+	New-Item -ItemType Directory -Force -Path "./$($company.Value)\" | Out-Null
 
 	# Detalji pravnog lica
 	Write-Host "`nDownload detalja pravnog lica"
 	$response = Invoke-RestMethod "https://eprijava.tax.gov.me/TaxisPortal/TaxPayerCompanies/Details?PIB=$($pib)" -Method 'POST' -Headers $headers
-	Out-File -FilePath ".\$($company.Value)\$($pib).htm" -InputObject $response -Encoding UTF8
+	Out-File -FilePath "./$($company.Value)\$($pib).htm" -InputObject $response -Encoding UTF8
 
 	# Pretraga liste finansijskih izvjestaja
 	Write-Host "`nPretraga liste finansijskih izvjestaja"
@@ -68,23 +77,23 @@ foreach ($company in $companies.GetEnumerator()) {
 		$year = $finStatement.Year
 		Write-Host "Download izvjestaja br. $($no) za godinu $($year)"
 		$response = Invoke-RestMethod "https://eprijava.tax.gov.me/TaxisPortal/FinancialStatement/Details?rbr=$($no)" -Method 'POST' -Headers $headers
-		
+
 		# Izvjestaji ce biti sacuvani u formatu: <PIB>-<GODINA>.htm
-		Out-File -FilePath ".\$($company.Value)\$($pib)-$($year).htm" -InputObject $response -Encoding UTF8
-		
-		$content = [IO.File]::ReadAllText(".\$($company.Value)\$($pib)-$($year).htm")
-		
+		Out-File -FilePath "./$($company.Value)\$($pib)-$($year).htm" -InputObject $response -Encoding UTF8
+
+		$content = [IO.File]::ReadAllText("./$company.Value\$pib-$year.htm")
+
 		# Pretraga podatka: totalIncome
 		$totalIncome = 0
-		$pattern = '(?:(PRIHODI|Prihodi).+\r\n.+(201).+\r\n.+\r\n[^>]+>)(?<totalIncome>\d+)(?:</td>)'
+		$pattern = '(?:(PRIHODI|Prihodi).+\r\n?|\n.+(201).+\r\n?|\n.+\r\n?|\n[^>]+>)(?<totalIncome>\d+)(?:</td>)'
 		$result = [regex]::Matches($content, $pattern)
 		if ($result -ne $null) {
 			$totalIncome = $result[0].Groups['totalIncome'].Value -as [int]
 		}
-		
+
 		# Pretraga podatka: profit
 		$profit = 0
-		$pattern = '(?:(Neto sveobuhvatni|NETO REZULTAT).+\r\n.+(260|232).+\r\n.+\r\n[^>]+>)(?<profit>\d+)(?:</td>)'
+		$pattern = '(?:(Neto sveobuhvatni|NETO REZULTAT).+\r\n?|\n.+(260|232).+\r\n?|\n.+\r\n?|\n[^>]+>)(?<profit>\d+)(?:</td>)'
 		$result = [regex]::Matches($content, $pattern)
 		if ($result -ne $null) {
 			$profit = $result[0].Groups['profit'].Value -as [int]
@@ -92,28 +101,28 @@ foreach ($company in $companies.GetEnumerator()) {
 
 		# Pretraga podatka: employeeCount
 		$employeeCount = 0
-		$pattern = '(?:(broj zaposlenih).+\r\n.+(001).+\r\n.+\r\n[^>]+>)(?<employeeCount>\d+)(?:</td>)'
+		$pattern = '(?:(broj zaposlenih).+\r\n?|\n.+(001).+\r\n?|\n.+\r\n?|\n[^>]+>)(?<employeeCount>\d+)(?:</td>)'
 		$result = [regex]::Matches($content, $pattern)
 		if ($result -ne $null) {
 			$employeeCount = $result[0].Groups['employeeCount'].Value -as [int]
 		}
-		
+
 		# Pretraga podatka: netPayCosts i kalkulacija averagePay
 		$netPayCosts = 0
 		$averagePay = 0
 
-		$pattern = '(?:(naknada zarada).+\r\n.+(212).+\r\n.+\r\n[^>]+>)(?<netPayCosts>\d+)(?:</td>)'
+		$pattern = '(?:(naknada zarada).+\r\n?|\n.+(212).+\r\n?|\n.+\r\n?|\n[^>]+>)(?<netPayCosts>\d+)(?:</td>)'
 		$result = [regex]::Matches($content, $pattern)
 		if ($result -ne $null) {
 			$netPayCosts = $result[0].Groups['netPayCosts'].Value -as [int]
 			$averagePay = $netPayCosts / $employeeCount / 12
 		}
-		
+
 		# Upis rezultata u Results.csv fajl
-		Add-Content -Path ".\Results.csv" -Value """$($company.Value)"", $($year), $($totalIncome), $($profit), $($employeeCount), $($netPayCosts), $($averagePay)"
+		Add-Content -Path "./Results.csv" -Value """$($company.Value)"", $($year), $($totalIncome), $($profit), $($employeeCount), $($netPayCosts), $($averagePay)"
 
 	}
-	
+
 }
 
 Write-Host "`nGotovo."
